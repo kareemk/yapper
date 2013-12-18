4c1ee0b446608ca02736dfc943af848d4b3637cf
Kareem Kouddous <kareemknyc@gmail.com>
2013-12-17 21:54:39 -0500

    Upgrading Pods

---
+++ vendor/Pods/Pods-NSData+MD5Digest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NSData_MD5Digest : NSObject
+@end
+@implementation PodsDummy_Pods_NSData_MD5Digest
+@end
---
+++ vendor/Pods/Pods-NanoStore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NanoStore : NSObject
+@end
+@implementation PodsDummy_Pods_NanoStore
+@end
