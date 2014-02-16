822e55bcede1a37160529db13716d7ab034c9d33
Kareem Kouddous <kareemknyc@gmail.com>
2014-02-16 17:09:42 -0500

    Pods

--- vendor/Pods/Headers/____Pods-environment.h
+++ vendor/Pods/Headers/____Pods-environment.h
@@ -6,12 +6,6 @@
 // project.
 
 
-// AFNetworking
-#define COCOAPODS_POD_AVAILABLE_AFNetworking
-#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
-#define COCOAPODS_VERSION_MINOR_AFNetworking 3
-#define COCOAPODS_VERSION_PATCH_AFNetworking 3
-
 // CocoaLumberjack
 #define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
 #define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 1
@@ -23,12 +17,6 @@
 #define COCOAPODS_VERSION_MAJOR_NSData_MD5Digest 1
 #define COCOAPODS_VERSION_MINOR_NSData_MD5Digest 0
 #define COCOAPODS_VERSION_PATCH_NSData_MD5Digest 0
-
-// Reachability
-#define COCOAPODS_POD_AVAILABLE_Reachability
-#define COCOAPODS_VERSION_MAJOR_Reachability 3
-#define COCOAPODS_VERSION_MINOR_Reachability 1
-#define COCOAPODS_VERSION_PATCH_Reachability 1
 
 // YapDatabase
 #define COCOAPODS_POD_AVAILABLE_YapDatabase
