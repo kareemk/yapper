a2455e985c6e48acf0e99676d52e4f7f8b2bc122
Kareem Kouddous <kareemknyc@gmail.com>
2014-11-26 18:08:25 -0500

    Updating to latest cocoapod

--- lib/yapper.rb
+++ lib/yapper.rb
@@ -15,7 +15,7 @@ Motion::Project::App.setup do |app|
 
   app.pods do
     pod 'YapDatabase'      ,'~> 2.5.2'
-    pod 'CocoaLumberjack'  ,'~> 1.6.5'
+    pod 'CocoaLumberjack'  ,'~> 1.8.1'
     pod 'NSData+MD5Digest' ,'~> 1.0.0'
   end
 
